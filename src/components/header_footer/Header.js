0e866331d6146fa4e62e013a4c29c828f6dc93f5
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-26 23:36:05 -0400

    11_Finishing the Header with transition

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -7,19 +7,34 @@ import SideDrawer from './SideDrawer';
 
 export default class Header extends Component {
   state = {
-    drawerOpen: false
+    drawerOpen: false,
+    headerShow: false
   };
   toggleDrawer = value => {
     this.setState({
       drawerOpen: value
     });
   };
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  handleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerShow:true
+      })
+    } else {
+      this.setState({
+        headerShow:false
+      })
+    }
+  }
   render() {
     return (
       <AppBar
         position="fixed"
         style={{
-          backgroundColor: '#2f2f2f',
+          backgroundColor: this.state.headerShow ?'#2f2f2f': 'transparent',
           boxShadow: 'none',
           padding: '10px 0px'
         }}
