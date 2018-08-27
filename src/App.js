217cd3ecc34212f97d67d2cf92aeb51729bce60c
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-26 22:43:22 -0400

    09_Creating the Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
+
+// Components
+import Header from './components/header_footer/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>App works</div>
+        <Header />
       </div>
     );
   }
