73304cdce9656d17b22dca423d71a07759c487c8
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-05 22:25:39 -0400

    18_Button reusable component

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
+import MyButton from '../utils/MyButton';
 
 class Discount extends Component {
   state = {
@@ -40,7 +41,12 @@ class Discount extends Component {
                 labore quos quod nisi, deleniti unde, neque accusamus itaque
                 praesentium rerum odit provident! Autem, tenetur?
               </p>
-              <button>Button</button>
+              <MyButton
+              text="Purchase tickets"
+              bck="#ffa800"
+              color="#ffffff"
+              link="http://google.com"
+              />
             </div>
           </Slide>
         </div>
