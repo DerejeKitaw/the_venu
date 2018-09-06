74086a448d9be2adaff1904ad0b7bd3935902288
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-05 22:02:29 -0400

    17_Highlight section

---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi qui
+          voluptatum pariatur at nam molestias eligendi, ullam esse repudiandae,
+          alias nobis aliquid libero obcaecati? Atque rem molestiae quidem sequi
+          illum.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi qui
+          voluptatum pariatur at nam molestias eligendi, ullam esse repudiandae,
+          alias nobis aliquid libero obcaecati? Atque rem molestiae quidem sequi
+          illum.
+        </div>
+      </div>
+    </Fade>
+  );
+};
+
+export default Description;
