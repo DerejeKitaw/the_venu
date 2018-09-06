4f95e6e78e9ac215bb26db9384b4a15a87486a44
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-05 23:35:41 -0400

    21_Adding a Scroll To side drawew

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
+import { Element } from 'react-scroll';
 
 // Components
 import Header from './components/header_footer/Header';
@@ -13,14 +14,27 @@ import Footer from './components/location/Footer';
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height:"1500px", background:'cornflowerblue'}}>
+      <div
+        className="App"
+        style={{ height: '1500px', background: 'cornflowerblue' }}
+      >
         <Header />
-        <Featured/>
-        <VenueNfo/>
-        <Highlights/>
-        <Pricing/>
-        <Location/>
-        <Footer/>
+        <Element name="Featured">
+          <Featured />
+        </Element>
+        <Element name="VenueNfo">
+          <VenueNfo />
+        </Element>
+        <Element name="Highlights">
+          <Highlights />
+        </Element>
+        <Element name="Pricing">
+          <Pricing />
+        </Element>
+        <Element name="Location">
+          <Location />
+        </Element>
+        <Footer />
       </div>
     );
   }
